69ff3d11799e8fa9dcb421b821852f6e501d1e12
Richard BOLLET <richard_bollet@hotmail.fr>
2016-10-10 22:06:08 +0200

    Update : Patch urls with slug category Add : a switcher button in menu to choose language FR or EN (default EN in config.yml and routing.yml) Add : redirecting a root path to that of a default locale (app/config/routing.yml)

--- src/Spectasonic/Front/CoreBundle/Controller/DefaultController.php
+++ src/Spectasonic/Front/CoreBundle/Controller/DefaultController.php
@@ -4,28 +4,42 @@ namespace Spectasonic\Front\CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class DefaultController extends Controller {
 
     public function indexAction() {
         $em = $this->getDoctrine()->getManager();
-        
+
         $homepage = $em
                 ->getRepository('SpectasonicBackCoreBundle:Homepage')
                 ->getFrontConfigurationHomepage(1);
 
-   
+
         /* Mylène
          * $repository = $this
-                ->getDoctrine()
-                ->getManager()
-                ->getRepository('SpectasonicBackBlogBundle:Slider');
-        
-        $slider = $repository->getSlider(4);*/
-                
-        
+          ->getDoctrine()
+          ->getManager()
+          ->getRepository('SpectasonicBackBlogBundle:Slider');
+
+          $slider = $repository->getSlider(4); */
+
+
         return $this->render('SpectasonicFrontCoreBundle:Default:index.html.twig', array('homepage' => $homepage));
     }
- 
+
+    public function selectLangAction(Request $request, $_langue = null) {
+        $urlReferer = $request->headers->get('referer');
+        $pattern = '#/fr/#';
+        if (preg_match($pattern, $urlReferer)) {
+            $return = preg_replace($pattern, '/en/', $urlReferer);
+        }
+        $pattern = '#/en/#';
+        if (preg_match($pattern, $urlReferer)) {
+            $return = preg_replace($pattern, '/fr/', $urlReferer);
+        }
+        return $this->redirect($return);
+    }
 
 }
